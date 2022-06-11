7f97d7c1ab6e46ceb155f31de2215575989a3e9c
arnoldmn <arnoldnzibuk@gmail.com>
2022-06-11 17:36:19 +0300

    Work header css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import "./header.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBed, faCab, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import { faBed, faCab, faPerson, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import { faCalendarDays } from '@fortawesome/free-regular-svg-icons'
 const Header = () => {
 
     return (
@@ -27,6 +28,26 @@ const Header = () => {
                     <div className="headerListItem">
                         <FontAwesomeIcon icon={faTaxi} />
                         <span>Airport taxis</span>
+                    </div>
+                </div>
+                <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
+                <p className="headerDesc">
+                    Get rewarded for your travels  -  unlock instant savings of 10% or more with a free BnBooking.com account
+                </p>
+                <button className="headerBtn">Sign in / Register</button>
+                <div className="headerSearch">
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faBed} className="headerIcon" />
+                        <input type="text" name="" id="" placeholder='where are you going?'
+                            className='headerSearchInput' />
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+                        <span className='headerSearchText'>date to date</span>
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+                        <span className='headerSearchText'>2 adults 2 children 1 room</span>
                     </div>
                 </div>
             </div>
