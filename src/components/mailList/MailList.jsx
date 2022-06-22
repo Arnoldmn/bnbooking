90908746bca8a84aeed08bf3b0f824be8eb67929
arnoldmn <arnoldnzibuk@gmail.com>
2022-06-22 18:11:58 +0300

    Mail list

---
+++ src/components/mailList/MailList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./mailList.css"
+
+export const MailList = () => {
+  return (
+    <div className='mail'>
+        <h1 className="mailTitle">
+            Save time, save money!
+        </h1>
+        <span className="mailDesc">Receive our newsletter and we will send the best deals to you!</span>
+        <div className="mailInputContainer">
+            <input type="text" placeholder='johndoe@example.com' />
+            <button>Subscribe</button>
+        </div>
+    </div>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Featured from '../../components/featured/Featured'
 import { FeaturedProperties } from '../../components/featuredProperties/FeaturedProperties'
 import Header from '../../components/header/Header'
+import { MailList } from '../../components/mailList/MailList'
 import Navbar from '../../components/navbar/Navbar'
 import {  PropertyList } from '../../components/propertiesList/PropertyList'
 import "./home.css"
@@ -17,6 +18,7 @@ const Home = () => {
         <PropertyList />
         <h className="homeTitle">Cities guests love</h>
         <FeaturedProperties />
+        <MailList />
       </div>
     </div>
   )
