6eb0a2ab32f35465b652cefc281f92615d34fcf3
arnoldmn <arnoldnzibuk@gmail.com>
2022-06-22 19:45:12 +0300

    Working the footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import "./footer.css"
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="fLists">
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Regions</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">District</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+                <li className="fListItem">Resorts</li>
+                <li className="fListItem">Condos</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Regions</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">District</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+                <li className="fListItem">Resorts</li>
+                <li className="fListItem">Condos</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Regions</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">District</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+                <li className="fListItem">Resorts</li>
+                <li className="fListItem">Condos</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Regions</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">District</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+                <li className="fListItem">Resorts</li>
+                <li className="fListItem">Condos</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Regions</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">District</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+                <li className="fListItem">Resorts</li>
+                <li className="fListItem">Condos</li>
+            </ul>
+        </div>
+        <div className="fText">Copyright &copy; 2022 BnBooking</div>
+    </div>
+  )
+}
