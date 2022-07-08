dcf770209f7f63a187dd630a5077079fac4c3ff8
arnoldmn <arnoldnzibuk@gmail.com>
2022-07-08 11:37:19 +0300

    Fix errors with import && add jwt lib

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,9 +1,9 @@
-import User from "../models/User"
+import User from "../models/User.js"
 import bcrypt from 'bcryptjs'
 
 export const register = async (req, res, next) => {
     try {
-        const salt =  bcrypt.genSaltsync(10)
+        const salt = bcrypt.genSaltsync(10)
         const hash = bcrypt.hashSync(req.body.password, salt)
 
         const newUser = User({
@@ -15,7 +15,7 @@ export const register = async (req, res, next) => {
 
         await newUser.save()
         res.status(200).send("User been created")
-        
+
     } catch (err) {
         next(err)
     }
@@ -23,10 +23,15 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
     try {
 
-       const user = User.findOne({username: re.body.username})
-       if(!user) return next(createError(404, "User not found!"))
-        res.status(200).send("User been created")
-        
+        const user = await User.findOne({ username: req.body.username })
+        if (!user) return next(createError(404, "User not found!"))
+
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+        if (!isPasswordCorrect) return next(createError(404, "Password is wrong!"))
+
+        const { password, isAdmin, ...otherDrtails } = user._doc
+        res.status(200).json(...otherDrtails)
+
     } catch (err) {
         next(err)
     }
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { login, register } from "../controllers/auth_controller";
+import { login, register } from "../controllers/auth_controller.js";
 
 const router = express.Router();
 
